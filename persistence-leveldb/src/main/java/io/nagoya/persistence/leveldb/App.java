a64a0d5ec9a4a145b3fbedf4e167a389b01c49aa
yellow013 <wk9988@gmail.com>
2019-11-09 19:53:24 +0800

    update leveldb

---
+++ persistence-leveldb/src/main/java/io/nagoya/persistence/leveldb/App.java
@@ -0,0 +1,13 @@
+package io.nagoya.persistence.leveldb;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
