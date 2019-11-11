4f262d5ac6492fcc5a2626a4567ab916319837a1
yellow013 <wk9988@gmail.com>
2019-11-11 14:26:06 +0800

    update leveldb

--- persistence-leveldb/src/main/java/io/nagoya/persistence/leveldb/UseExample.java
+++ persistence-leveldb/src/main/java/io/nagoya/persistence/leveldb/UseExample.java
@@ -4,10 +4,10 @@ package io.nagoya.persistence.leveldb;
  * Hello world!
  *
  */
-public class App 
-{
-    public static void main( String[] args )
-    {
-        System.out.println( "Hello World!" );
-    }
+public class UseExample {
+	
+	public static void main(String[] args) {
+		System.out.println("Hello World!");
+	}
+	
 }
