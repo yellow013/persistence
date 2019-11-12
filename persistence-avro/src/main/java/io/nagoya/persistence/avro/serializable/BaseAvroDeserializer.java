4213237feda1163158f4fdb8e57ad9bc66d1386f
yellow013 <wk9988@gmail.com>
2019-11-12 19:52:31 +0800

    update avro

---
+++ persistence-avro/src/main/java/io/nagoya/persistence/avro/serializable/BaseAvroDeserializer.java
@@ -0,0 +1,25 @@
+package io.nagoya.persistence.avro.serializable;
+
+import org.apache.avro.specific.SpecificDatumReader;
+import org.slf4j.Logger;
+
+import io.ffreedom.common.log.CommonLoggerFactory;
+
+public abstract class BaseAvroDeserializer<T> {
+
+	protected Logger logger = CommonLoggerFactory.getLogger(getClass());
+
+	private SpecificDatumReader<T> datumReader;
+	
+	protected final SpecificDatumReader<T> getDatumReader(Class<T> tClass) {
+		if(datumReader == null) {
+			datumReader = initDatumReader(tClass);
+		}
+		return datumReader;
+	}
+
+	private final SpecificDatumReader<T> initDatumReader(Class<T> tClass) {
+		return new SpecificDatumReader<>(tClass);
+	}
+
+}
