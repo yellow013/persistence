63210498455f61a9c2df6b2b50348cb279141774
yellow013 <wk9988@gmail.com>
2019-11-12 19:42:23 +0800

    update rocksdb

---
+++ persistence-rocksdb/src/main/java/io/nagoya/persistence/rocksdb/RocksContainer.java
@@ -0,0 +1,13 @@
+package io.nagoya.persistence.rocksdb;
+
+public class RocksContainer<K extends RocksKey, V extends RocksValue> {
+	
+	public static void main(String[] args) {
+		
+		
+		
+		
+		
+	}
+
+}
---
+++ persistence-rocksdb/src/main/java/io/nagoya/persistence/rocksdb/RocksKey.java
@@ -0,0 +1,7 @@
+package io.nagoya.persistence.rocksdb;
+
+public interface RocksKey {
+
+	byte[] bytesKey();
+
+}
