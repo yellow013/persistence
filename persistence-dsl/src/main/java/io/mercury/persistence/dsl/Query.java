1521f02b8699626e6e73498638e1d7ae7e866f60
yellow013 <wk9988@gmail.com>
2019-12-03 20:37:52 +0800

    update dsl

---
+++ persistence-dsl/src/main/java/io/mercury/persistence/dsl/Query.java
@@ -0,0 +1,13 @@
+package io.mercury.persistence.dsl;
+
+/**
+ * Hello world!
+ *
+ */
+public class Query 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
