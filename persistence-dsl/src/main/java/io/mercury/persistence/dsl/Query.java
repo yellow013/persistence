9bb1d4eccd9b4cf8b4590d51e5e161a273b15d71
yellow013 <wk9988@gmail.com>
2019-12-06 19:43:01 +0800

    update dsl project

--- persistence-dsl/src/main/java/io/mercury/persistence/dsl/Query.java
+++ persistence-dsl/src/main/java/io/mercury/persistence/dsl/Query.java
@@ -1,13 +1,9 @@
 package io.mercury.persistence.dsl;
 
-/**
- * Hello world!
- *
- */
-public class Query 
-{
-    public static void main( String[] args )
-    {
-        System.out.println( "Hello World!" );
-    }
+public class Query {
+
+	public static void main(String[] args) {
+		System.out.println("Hello World!");
+	}
+
 }
