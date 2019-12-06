11f01e65197bb558503d04edcd81088eeaa24c1d
yellow013 <wk9988@gmail.com>
2019-12-06 19:43:19 +0800

    rocksdb

--- persistence-rocksdb/src/main/java/io/mercury/persistence/rocksdb/RocksDBOptions.java
+++ persistence-rocksdb/src/main/java/io/mercury/persistence/rocksdb/RocksDBOptions.java
@@ -1,6 +1,8 @@
 package io.mercury.persistence.rocksdb;
 
-import java.nio.file.Path;
+import static io.mercury.common.utils.StringUtil.fixPath;
+
+import java.io.File;
 
 import org.rocksdb.Options;
 
@@ -8,29 +10,57 @@ import io.mercury.common.sys.SysProperties;
 
 public final class RocksDBOptions {
 
+	private final String rootPath;
+	private final String folder;
+	private final File savePath;
+
 	private final Options options;
-	
-	private Path savePath;
 
 	private RocksDBOptions(Builder builder) {
+		this.rootPath = builder.rootPath;
+		this.folder = builder.folder;
+		this.savePath = new File(rootPath + "rocksdb/" + folder);
 		this.options = new Options();
 		options.setCreateIfMissing(builder.createIfMissing);
 		options.setCreateMissingColumnFamilies(builder.createMissingColumnFamilies);
 
 	}
-	
-	private static final String FixedFolder = "rocksdb/";
-	
-	public static final String DefaultRootPath = SysProperties.JAVA_IO_TMPDIR + "/";
-	public static final String DefaultFolder = "default/";
+
+	public String rootPath() {
+		return rootPath;
+	}
+
+	public String folder() {
+		return folder;
+	}
+
+	public File savePath() {
+		return savePath;
+	}
+
+	public Options options() {
+		return options;
+	}
 
 	public static Builder builder() {
 		return new Builder();
 	}
 
 	public static class Builder {
+
+		private String rootPath = SysProperties.JAVA_IO_TMPDIR + "/";
+		private String folder = "default/";
+
 		private boolean createIfMissing = true;
 		private boolean createMissingColumnFamilies = false;
+
+		public void rootPath(String rootPath) {
+			this.rootPath = fixPath(rootPath);
+		}
+
+		public void folder(String folder) {
+			this.folder = fixPath(folder);
+		}
 
 		public boolean createIfMissing() {
 			return createIfMissing;
