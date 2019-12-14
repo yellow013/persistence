6109afb8e6ec728b27c8bb7d5c6838dad7da4e93
yellow013 <wk9988@gmail.com>
2019-12-14 19:12:01 +0800

    update new set impl

---
+++ persistence-chronicle/src/main/java/io/mercury/persistence/chronicle/set/ChronicleSetKeeperOfDate.java
@@ -0,0 +1,21 @@
+package io.mercury.persistence.chronicle.set;
+
+import java.time.LocalDate;
+
+import io.mercury.common.annotations.lang.MayThrowsRuntimeException;
+import io.mercury.common.datetime.DateTimeUtil;
+import io.mercury.persistence.chronicle.exception.ChronicleIOException;
+import net.openhft.chronicle.set.ChronicleSet;
+
+public final class ChronicleSetKeeperOfDate<K> extends ChronicleSetKeeper<K > {
+
+	public ChronicleSetKeeperOfDate(ChronicleSetConfigurator<K > options) {
+		super(options);
+	}
+
+	@MayThrowsRuntimeException(ChronicleIOException.class)
+	public ChronicleSet<K > acquire(LocalDate date) throws ChronicleIOException {
+		return super.acquire(String.valueOf(DateTimeUtil.date(date)));
+	}
+
+}
