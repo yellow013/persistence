dce81f16e5f4801539549e3c0bf9323c34f8f83c
yellow013 <wk9988@gmail.com>
2020-03-12 20:56:45 +0800

    update pom.xml

--- persistence-sqlite/src/main/java/io/mercury/persistence/sqlite/App.java
+++ persistence-sqlite/src/main/java/io/mercury/persistence/sqlite/App.java
@@ -1,13 +1,10 @@
 package io.mercury.persistence.sqlite;
 
-/**
- * Hello world!
- *
- */
 public class App {
-	
+
 	public static void main(String[] args) {
+		
 		System.out.println("Hello World!");
 	}
-	
+
 }
