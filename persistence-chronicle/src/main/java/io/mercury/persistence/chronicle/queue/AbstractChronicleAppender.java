bf0d7ea6054a0c5440ba66247a37b0863c5cd5a5
yellow013 <wk9988@gmail.com>
2020-04-04 17:35:00 +0800

    update appender

--- persistence-chronicle/src/main/java/io/mercury/persistence/chronicle/queue/AbstractChronicleAppender.java
+++ persistence-chronicle/src/main/java/io/mercury/persistence/chronicle/queue/AbstractChronicleAppender.java
@@ -9,22 +9,21 @@ import org.slf4j.Logger;
 import io.mercury.common.annotation.lang.MayThrowsRuntimeException;
 import io.mercury.common.annotation.lang.ProtectedAbstractMethod;
 import io.mercury.persistence.chronicle.exception.ChronicleWriteException;
-import net.openhft.chronicle.core.io.Closeable;
+import io.mercury.persistence.chronicle.queue.AbstractChronicleQueue.CloseableChronicleAccessor;
 import net.openhft.chronicle.queue.ExcerptAppender;
 
-public abstract class AbstractChronicleAppender<T> implements Runnable, Closeable {
+public abstract class AbstractChronicleAppender<T> extends CloseableChronicleAccessor implements Runnable {
 
 	private final String appenderName;
 
 	protected final Logger logger;
 	protected final ExcerptAppender excerptAppender;
 
-	private volatile boolean isClose = false;
-
 	private Supplier<T> supplier;
 
-	AbstractChronicleAppender(String appenderName, Logger logger, ExcerptAppender excerptAppender,
+	AbstractChronicleAppender(long allocationNo, String appenderName, Logger logger, ExcerptAppender excerptAppender,
 			Supplier<T> supplier) {
+		super(allocationNo);
 		this.appenderName = appenderName;
 		this.logger = logger;
 		this.excerptAppender = excerptAppender;
@@ -48,7 +47,7 @@ public abstract class AbstractChronicleAppender<T> implements Runnable, Closeabl
 	}
 
 	@ProtectedAbstractMethod
-	protected abstract void append0(T t);
+	protected abstract void append0(@Nonnull T t);
 
 	/**
 	 * append element to queue tail.
@@ -57,14 +56,14 @@ public abstract class AbstractChronicleAppender<T> implements Runnable, Closeabl
 	 * @throws ChronicleWriteException
 	 */
 	@MayThrowsRuntimeException(ChronicleWriteException.class)
-	public void append(@Nonnull T t) throws ChronicleWriteException {
+	public void append(@Nonnull T t) throws IllegalStateException, ChronicleWriteException {
+		if (isClose) {
+			throw new IllegalStateException("Unable to append data, Chronicle queue is closed");
+		}
 		try {
-			if (t != null) {
-				if (!isClose)
-					append0(t);
-				else
-					logger.warn("appenderName -> {} the swap queue is closed.");
-			} else
+			if (t != null)
+				append0(t);
+			else
 				logger.warn("appenderName -> {} received null object, Not written to queue.");
 		} catch (Exception e) {
 			throw new ChronicleWriteException(e.getMessage(), e);
@@ -75,17 +74,21 @@ public abstract class AbstractChronicleAppender<T> implements Runnable, Closeabl
 	public void run() {
 		if (supplier != null) {
 			for (;;) {
-				T t = supplier.get();
-				append(t);
+				if (isClose) {
+					logger.info("Chronicle queue is closed, Thread exit");
+					break;
+				} else {
+					T t = supplier.get();
+					append(t);
+				}
 			}
 		} else {
-			logger.warn("supplier is null, thread exit.");
-			return;
+			logger.warn("Supplier is null, Thread exit");
 		}
 	}
 
-	public void close() {
-		isClose = true;
+	protected void close0() {
+
 	}
 
 }
