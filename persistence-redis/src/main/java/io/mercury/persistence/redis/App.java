c07d1f7a0ad7668dd93301244bcf8828ce54b430
yellow013 <wk9988@gmail.com>
2020-06-06 17:37:48 +0800

    update redis

--- persistence-redis/src/main/java/io/mercury/persistence/redis/App.java
+++ persistence-redis/src/main/java/io/mercury/persistence/redis/App.java
@@ -1,13 +1,24 @@
 package io.mercury.persistence.redis;
 
+import io.lettuce.core.RedisClient;
+import io.lettuce.core.api.StatefulRedisConnection;
+import io.lettuce.core.api.sync.RedisCommands;
+
 /**
  * Hello world!
  *
  */
-public class App 
-{
-    public static void main( String[] args )
-    {
-        System.out.println( "Hello World!" );
-    }
+public class App {
+
+	public static void main(String[] args) {
+		RedisClient redisClient = RedisClient.create("redis://password@localhost:6379/0");
+		StatefulRedisConnection<String, String> connection = redisClient.connect();
+		RedisCommands<String, String> syncCommands = connection.sync();
+
+		syncCommands.set("key", "Hello, Redis!");
+
+		connection.close();
+		redisClient.shutdown();
+	}
+
 }
